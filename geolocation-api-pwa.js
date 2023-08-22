faa2b406563c2c96359c263f4d53e558ebec650b
kbstt <80958930+kbstt@users.noreply.github.com>
2023-08-22 11:23:50 +0800

    Update geolocation-api-pwa.js

--- geolocation-api-pwa.js
+++ geolocation-api-pwa.js
@@ -12,7 +12,7 @@ function requestLocation(){
 
 function createMap(){
   // Create a default map
-  window.demoMap = L.map("map").setView([34.034, 118.23], 13);
+  window.demoMap = L.map("map").setView([48.8584, 2.2945], 5);
   
   // Add a tile layer from OpenStreetMap's default tile server
   L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png?{foo}', {foo: 'bar', attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'}).addTo(window.demoMap);
