0f3aa9acdccbcf53b709d5bca8f71b2d81761a80
kbstt <80958930+kbstt@users.noreply.github.com>
2023-08-22 23:15:28 -0500

    Update contact-picker-pwa.js

--- contact-picker-pwa.js
+++ contact-picker-pwa.js
@@ -3,7 +3,7 @@ async function pickContacts(){
      alert("Your device does not support the Contact Picker API. Open this page on Android Chrome to give it a try!");
   }
   else {
-      //first we ask the browser to tell us which properties the device support
+      //first we ask the browser to tell us which properties the device supports
       //available properties include 'name', 'tel', 'email', 'address', and 'icon'
       let propertiesAvailable = await navigator.contacts.getProperties();
 
