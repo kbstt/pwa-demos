4bc5e118c33cb7ea726187e12ef1a1402e85ddc7
kbstt <80958930+kbstt@users.noreply.github.com>
2023-08-24 00:56:58 -0500

    Create web-bluetooth-api-pwa.js

---
+++ web-bluetooth-api-pwa.js
@@ -0,0 +1,12 @@
+function connectToBluetoothDevice(){
+  if (!navigator.bluetooth || !navigator.bluetooth.requestDevice){
+    alert("Your device does not support the Web Bluetooth API. Try again on Chrome on Desktop or Android!");
+  }
+  else {
+    //in this example, we'll simply allow connecting to any device nearby
+    //in a real-life example, you'll probably want to use filter so that your app only connects to certain types of devices (e.g. a heart rate monitor)
+    //more on this here: https://developer.mozilla.org/en-US/docs/Web/API/Bluetooth/requestDevice
+    let device = await navigator.bluetooth.requestDevice({acceptAllDevices: true});
+    console.log(device);
+  }
+}
