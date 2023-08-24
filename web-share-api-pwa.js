fd1aa39c3445d306b189b772370d916d6551341b
kbstt <80958930+kbstt@users.noreply.github.com>
2023-08-23 23:03:14 -0500

    Update web-share-api-pwa.js

--- web-share-api-pwa.js
+++ web-share-api-pwa.js
@@ -1,13 +1,14 @@
 function shareContent(){
+  let url = document.getElementById("content-url").value;
+  let title = document.getElementById("content-title").value;
+  let text = document.getElementById("content-text").value;
+  let data = {url: url, text: text, title: title};
+  console.log(data);
+  
   if (!navigator.share){
     alert("Your device does not support the Web Share API. Try on an iPhone or Android phone!");
   }
   else {
-    let url = document.getElementById("content-url").value;
-    let title = document.getElementById("content-title").value;
-    let text = document.getElementById("content-text").value;
-    let data = {url: url, text: text, title: title};
-    console.log(data);
     navigator.share(data);
   }
 } 
