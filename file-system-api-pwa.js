8d389aa50cc825a7555d15da974370b06f1b6d4c
kbstt <80958930+kbstt@users.noreply.github.com>
2023-08-23 20:19:31 -0500

    Update file-system-api-pwa.js

--- file-system-api-pwa.js
+++ file-system-api-pwa.js
@@ -1,25 +1,30 @@
 async function chooseAFile() {
-  //here you specify the type of files you want to allow
-  let options = {
-    types: [{
-      description: "Images", 
-      accept: {
-          "image/*": [".png", ".gif", ".jpeg", ".jpg"],
-          "text/*": [".txt", ".json"],
-          "application/*": [".json"],
-      },
-    }],
-    excludeAcceptAllOption: true,
-    multiple: false,
-  };
-  
-  // Open file picker and choos ea file
-  let fileHandle = await window.showOpenFilePicker(options);
-  console.log(fileHandle);
-  
-  // get the content of the file
-  let file = await fileHandle[0].getFile();
-  console.log(file); 
+  if (!window.showOpenFilePicker){
+      alert("Your current device does not support the File System API. Try again on desktop Chrome!");
+  }
+  else {
+    //here you specify the type of files you want to allow
+    let options = {
+      types: [{
+        description: "Images", 
+        accept: {
+            "image/*": [".png", ".gif", ".jpeg", ".jpg"],
+            "text/*": [".txt", ".json"],
+            "application/*": [".json"],
+        },
+      }],
+      excludeAcceptAllOption: true,
+      multiple: false,
+    };
+    
+    // Open file picker and choos ea file
+    let fileHandle = await window.showOpenFilePicker(options);
+    console.log(fileHandle);
+    
+    // get the content of the file
+    let file = await fileHandle[0].getFile();
+    console.log(file); 
+  }
 }
 
 function renderFile(){
