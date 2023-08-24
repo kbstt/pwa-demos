b0e822d76716263c2775002d9b96e43e59d60c1e
kbstt <80958930+kbstt@users.noreply.github.com>
2023-08-23 22:39:38 -0500

    Create web-share-api-pwa.js

---
+++ web-share-api-pwa.js
@@ -0,0 +1,12 @@
+function shareContent(){
+  if (!navigator.share){
+    alert("Your device does not support the Web Share API. Try on an iPhone or Android phone!");
+  }
+  else {
+    let url = document.getElementById("content-url").value;
+    let title = document.getElementById("content-title").value;
+    let text = document.getElementById("content-text").value;
+    let data = {url: url, text: text, title: title};
+    navigator.share(data);
+  }
+} 
