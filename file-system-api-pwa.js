9471de53da48e23513f9e3a4d75cd54878d91861
kbstt <80958930+kbstt@users.noreply.github.com>
2023-08-23 20:15:50 -0500

    Create file-system-api-pwa.js

---
+++ file-system-api-pwa.js
@@ -0,0 +1,27 @@
+async function chooseAFile() {
+  //here you specify the type of files you want to allow
+  let options = {
+    types: [{
+      description: "Images", 
+      accept: {
+          "image/*": [".png", ".gif", ".jpeg", ".jpg"],
+          "text/*": [".txt", ".json"],
+          "application/*": [".json"],
+      },
+    }],
+    excludeAcceptAllOption: true,
+    multiple: false,
+  };
+  
+  // Open file picker and choos ea file
+  let fileHandle = await window.showOpenFilePicker(options);
+  console.log(fileHandle);
+  
+  // get the content of the file
+  let file = await fileHandle.getFile();
+  console.log(file); 
+}
+
+function renderFile(){
+   console.log(file)
+}
