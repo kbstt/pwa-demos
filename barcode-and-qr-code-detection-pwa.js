145eebc1a26b4999ee0a2fbfa9a86e2182b55a3b
kbstt <80958930+kbstt@users.noreply.github.com>
2023-08-23 21:37:14 -0500

    Update barcode-and-qr-code-detection-pwa.js

--- barcode-and-qr-code-detection-pwa.js
+++ barcode-and-qr-code-detection-pwa.js
@@ -24,7 +24,7 @@ async function startBarcodeScanner() {
                         
             if (barcodes.length > 0) {
                 let barcodeData = barcodes[0].rawValue;
-                alert("Detected QR code linking to following URL: "+barcodeData); 
+                alert("Detected QR code with the following content: "+barcodeData); 
             };
           
            requestAnimationFrame(checkForQrCode);
