925e4ddadbc8cb7472e8556c0ffe08654dcec793
kbstt <80958930+kbstt@users.noreply.github.com>
2023-08-30 01:34:53 -0500

    Update speech-recognition-api-pwa.js

--- speech-recognition-api-pwa.js
+++ speech-recognition-api-pwa.js
@@ -1,9 +1,20 @@
-function startSpeechRecognition(){
-  let recognition = window.webkitSpeechRecognition ? new webkitSpeechRecognition() : new SpeechRecognition();
-  recognition.lang = "en-US";
-  recognition.interimResults = true;
-  recognition.start();
-  recognition.addEventListener("result", function(e){
-    console.log(e);
-  });
+function toggleSpeechRecognition(){
+  if (!window.webkitSpeechRecognition && ! window.SpeechRecognition){
+    alert("Your browser does not support the SpeechRecognition API");
+  }
+  else if (window.transcriptionInProgress){
+    window.transcriptionInProgress.stop();
+  }
+  else {
+    window.transcriptionInProgress = window.webkitSpeechRecognition ? new webkitSpeechRecognition() : new SpeechRecognition();
+    window.transcriptionInProgress.lang = "en-US";
+    window.transcriptionInProgress.interimResults = true;
+    window.transcriptionInProgress.start();
+    window.transcriptionInProgress.addEventListener("result", function(e){
+      console.log(e);
+    });
+    window.transcriptionInProgress.addEventListener("speechend", function(e){
+       window.transcriptionInProgress = null;
+    });
+  }
 }
