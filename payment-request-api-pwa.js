311ca3df8220eb06555122b584a7f3b73cde3088
kbstt <80958930+kbstt@users.noreply.github.com>
2023-08-29 20:43:44 -0500

    Create payment-request-api-pwa.js

---
+++ payment-request-api-pwa.js
@@ -0,0 +1,32 @@
+function startPayment(){  
+    let checkoutDetails = {
+      id: "pwa-demo-order",
+      displayItems: [
+        {label: "Progressier PWA Demo", amount: {currency: "USD", value: "1"}}
+      ],
+      total: { label: "Total", amount: {currency: "USD", value: "1"}}
+    };
+  
+    let paymentMethods = [
+      {
+        supportedMethods: "basic-card",
+        data: {
+          supportedNetworks: ["visa", "mastercard"]
+        }
+      }, 
+      {
+        supportedMethods: "https://apple.com/apple-pay",
+        data: { 
+          version: 3, 
+          merchantIdentifier: "progressier.com", 
+          merchantCapabilities: ["supports3DS", "supportsCredit", "supportsDebit"], 
+          supportedNetworks: ["amex", "discover", "masterCard", "visa", "maestro"],
+          countryCode: "US" 
+        }
+      }              
+    ];
+  
+    let paymentRequest = new PaymentRequest(paymentMethods, checkoutDetails);
+    let response = await request.show();
+    console.log(response);
+}
