8fee4f3968d4bf302c35ca411766aa78a423e1fd
kbstt <80958930+kbstt@users.noreply.github.com>
2023-08-29 21:00:00 -0500

    Update payment-request-api-pwa.js

--- payment-request-api-pwa.js
+++ payment-request-api-pwa.js
@@ -10,11 +10,38 @@ async function startPayment(){
     let paymentMethods = [
       {
         supportedMethods: "basic-card",
-        data: {
-          supportedNetworks: ["visa", "mastercard"]
-        }
       }, 
-     /* {
+    /*  {
+        supportedMethods: 'https://google.com/pay', 
+        data: {
+              environment: 'TEST',
+              apiVersion: 2,
+              apiVersionMinor: 0,
+              merchantInfo: {
+                // A merchant ID is available after approval by Google.
+                // @see {@link https://developers.google.com/pay/api/web/guides/test-and-deploy/integration-checklist}
+                // merchantId: '12345678901234567890',
+                merchantName: 'Example Merchant'
+              },
+              allowedPaymentMethods: [{
+                type: 'CARD',
+                parameters: {
+                  allowedAuthMethods: ["PAN_ONLY", "CRYPTOGRAM_3DS"],
+                  allowedCardNetworks: ["AMEX", "DISCOVER", "INTERAC", "JCB", "MASTERCARD", "VISA"]
+                },
+                tokenizationSpecification: {
+                  type: 'PAYMENT_GATEWAY',
+                  // Check with your payment gateway on the parameters to pass.
+                  // @see {@link https://developers.google.com/pay/api/web/reference/request-objects#gateway}
+                  parameters: {
+                    'gateway': 'example',
+                    'gatewayMerchantId': 'exampleGatewayMerchantId'
+                  }
+                }
+              }]
+            }
+      },
+     {
         supportedMethods: "https://apple.com/apple-pay",
         data: { 
           version: 3, 
@@ -23,7 +50,7 @@ async function startPayment(){
           supportedNetworks: ["amex", "discover", "masterCard", "visa", "maestro"],
           countryCode: "US" 
         }
-      }    */          
+      }   */     
     ];
   
     let paymentRequest = new PaymentRequest(paymentMethods, checkoutDetails);
