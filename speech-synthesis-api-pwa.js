6819c500e65141d8ea7e14b5bc73578fe6edca81
kbstt <80958930+kbstt@users.noreply.github.com>
2023-08-30 00:30:31 -0500

    Update speech-synthesis-api-pwa.js

--- speech-synthesis-api-pwa.js
+++ speech-synthesis-api-pwa.js
@@ -3,12 +3,10 @@ function synthesiseSpeech(){
     alert("Your device does not support the SpeechSynthesis API");
   }
   else {
-    let text = document.getElementById('text-to-read').value;
-    let voices = speechSynthesis.getVoices();
-    let voice = voices.find(o => o.voiceURI === document.getElementById('voice-choice').value) || voices[0];
+    let availableVoices = speechSynthesis.getVoices();
     let utterance = new SpeechSynthesisUtterance();
-    utterance.text = text;
-    utterance.voice = voice;
+    utterance.text = document.getElementById('text-to-read').value;
+    utterance.voice = availableVoices.find(o => o.voiceURI === document.getElementById('voice-choice').value) || availableVoices[0];
     utterance.pitch = document.getElementById('pitch').value;
     utterance.rate = document.getElementById('rate').value;
     speechSynthesis.speak(utterance);
