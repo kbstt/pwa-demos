68c4eeb520036c532216a914b75cf6ef512b3849
kbstt <80958930+kbstt@users.noreply.github.com>
2023-08-30 01:29:27 -0500

    Update speech-recognition-api-pwa.js

--- speech-recognition-api-pwa.js
+++ speech-recognition-api-pwa.js
@@ -1,7 +1,7 @@
 function startSpeechRecognition(){
   let recognition = window.webkitSpeechRecognition ? new webkitSpeechRecognition() : new SpeechRecognition();
   recognition.lang = "en-US";
-  recognition.interimResults = false;
+  recognition.interimResults = true;
   recognition.start();
   recognition.addEventListener("result", function(e){
     console.log(e);
