4c17ef9c35dbaea28fb0b4c7b12d342620653298
kbstt <80958930+kbstt@users.noreply.github.com>
2023-08-29 20:03:12 -0500

    Update pwa-vibration-api.js

--- pwa-vibration-api.js
+++ pwa-vibration-api.js
@@ -1,7 +1,6 @@
 let patterns = [
   2000, //vibrate one time for 2 seconds
   [2000, 1000, 2000, 1000, 2000, 1000, 2000],
-  [1000, 150, 1000, 150, 2000, 150, 1000, 150, 1000, 150], //vibrate "Super Mario Bros" theme
   [400, 200, 400, 200, 400, 200, 800, 200, 800, 200, 400, 200, 400, 200, 200, 200], //vibrate "Twinkle, Twinkle, Little Star"
   [150, 50, 150, 50, 300, 100, 150, 50, 150, 50, 300, 100, 150, 50, 150, 50], //vibrate "Super Mario Bros" theme
   [300, 200, 300, 200, 300, 400, 300, 200, 300, 200, 300, 400, 300, 200, 600, 200] //vibrate "Jingle Bells"
@@ -12,7 +11,6 @@ function vibrationPattern(index){
     alert("Your device does not support the Vibration API. Try on an Android phone!");
   }
   else {
-    console.log(patterns[index]);
     window.navigator.vibrate(patterns[index]);
   }
 }
