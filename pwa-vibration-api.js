b1f95ed202501ea5aadd490d3c89ccb6c313ff2a
kbstt <80958930+kbstt@users.noreply.github.com>
2023-08-29 19:08:48 -0500

    Update pwa-vibration-api.js

--- pwa-vibration-api.js
+++ pwa-vibration-api.js
@@ -1,16 +1,15 @@
 let patterns = [
   2000,
+  [100, 30, 100, 30, 100, 30, 200, 30, 200, 30, 200, 30, 100, 30, 100, 30, 100],
   [1000, 500, 1000, 500, 1000],
   [50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50]
 ];
 
 function vibrationPattern(index){
-  console.log(index);
   if (!window.navigator.vibrate){
     alert("Your device does not support the Vibration API. Try on an Android phone!");
   }
   else {
-    console.log(patterns[index]);
     window.navigator.vibrate(patterns[index]);
   }
 }
