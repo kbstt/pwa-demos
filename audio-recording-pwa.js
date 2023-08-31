588fcdb909be272f5996e8354bdc3ecb4f4be148
kbstt <80958930+kbstt@users.noreply.github.com>
2023-08-30 21:44:46 -0500

    Create audio-recording-pwa.js

---
+++ audio-recording-pwa.js
@@ -0,0 +1,11 @@
+function startRecording(){
+  try {
+    let stream = await navigator.mediaDevices.getUserMedia({audio: true, video: false });
+    let audioElement = document.getElementById('audio-element');
+    audioElement.src = stream;
+  }
+  catch(err){
+    console.log(err);
+  }
+}
+
