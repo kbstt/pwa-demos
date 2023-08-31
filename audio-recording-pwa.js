1706592e43d29838cb66b4337d6343e1b007b8c7
kbstt <80958930+kbstt@users.noreply.github.com>
2023-08-30 22:29:03 -0500

    Update audio-recording-pwa.js

--- audio-recording-pwa.js
+++ audio-recording-pwa.js
@@ -1,11 +1,29 @@
-async function startRecording(){
-  try {
-    let stream = await navigator.mediaDevices.getUserMedia({audio: true, video: false });
-    let audioElement = document.getElementById('audio-element');
-    audioElement.src = stream;
+async function recordAudio(){
+  if (window.recorder && window.recorder.state === "recording"){
+     window.recorder.stop();
   }
-  catch(err){
-    console.log(err);
+  else {
+    let toggle = document.getElementById('recording-button');
+    let stream = await navigator.mediaDevices.getUserMedia({audio: true, video: false });
+    window.recorder = new MediaRecorder(stream);
+
+    let chunks = [];
+    window.recorder.ondataavailable = function(event){
+      if (event.data.size <= 0) {return;}
+      chunks.push(event.data);
+    };
+
+    window.recorder.onstart = function(){
+      toggle.innerHTML = `<i class="fa fa-circle"></i>`;
+    };
+    
+    window.recorder.onstop = function(){
+      let blob = new Blob(chunks, { type: 'audio/mp3' });
+      toggle.innerHTML = `<i class="fa fa-square"></i>`;
+      document.getElementById('audio-element').src = URL.createObjectURL(blob);
+    };
+  
+    window.recorder.start();
   }
 }
 
