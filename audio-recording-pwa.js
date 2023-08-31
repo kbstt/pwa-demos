49983d36bd413aa23dc23354fa0ec09fc33ad774
kbstt <80958930+kbstt@users.noreply.github.com>
2023-08-30 21:52:21 -0500

    Update audio-recording-pwa.js

--- audio-recording-pwa.js
+++ audio-recording-pwa.js
@@ -1,4 +1,4 @@
-function startRecording(){
+async function startRecording(){
   try {
     let stream = await navigator.mediaDevices.getUserMedia({audio: true, video: false });
     let audioElement = document.getElementById('audio-element');
