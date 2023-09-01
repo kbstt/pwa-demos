4e95a0fd306922a2fecf3f4176b7ba4631212de4
kbstt <80958930+kbstt@users.noreply.github.com>
2023-08-31 19:51:21 -0500

    Update video-recording-pwa.js

--- video-recording-pwa.js
+++ video-recording-pwa.js
@@ -3,8 +3,12 @@ async function recordVideo(){
      window.recorder.stop();
   }
   else {
+    let videoEl = document.getElementById('video-element');
     let toggle = document.getElementById('recording-button');
     let stream = await navigator.mediaDevices.getUserMedia({audio: true, video: true });
+    videoEl.srcObject = stream;
+    videoEl.play();
+    
     window.recorder = new MediaRecorder(stream);
 
     let chunks = [];
@@ -20,7 +24,7 @@ async function recordVideo(){
     window.recorder.onstop = function(){
       let blob = new Blob(chunks, { type: 'video/mp4' });
       toggle.innerHTML = `<i class="fa fa-circle"></i>`;
-      document.getElementById('video-element').src = URL.createObjectURL(blob);
+      videoEl.src = URL.createObjectURL(blob);
       let tracks = stream.getTracks();
       tracks.forEach(track => track.stop());
     };
