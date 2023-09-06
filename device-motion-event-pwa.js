5f738728012b91c51ac11ba2840f44981e91f57d
kbstt <80958930+kbstt@users.noreply.github.com>
2023-09-06 00:55:39 -0500

    Update device-motion-event-pwa.js

--- device-motion-event-pwa.js
+++ device-motion-event-pwa.js
@@ -1,13 +1,13 @@
 window.addEventListener("deviceorientation", function(e){
-  document.getElementById('acceleration-x').innerHTML = e.acceleration.x+"m/s²";
-  document.getElementById('acceleration-y').innerHTML = e.acceleration.y+"m/s²";
-  document.getElementById('acceleration-z').innerHTML = e.acceleration.z+"m/s²";
+  document.getElementById('acceleration-x').innerHTML = e.acceleration.x.toFixed(2)+"m/s²";
+  document.getElementById('acceleration-y').innerHTML = e.acceleration.y.toFixed(2)+"m/s²";
+  document.getElementById('acceleration-z').innerHTML = e.acceleration.z.toFixed(2)+"m/s²";
 
-  document.getElementById('acceleration-gravity-x').innerHTML = e.accelerationIncludingGravity.x+"m/s²";
-  document.getElementById('acceleration-gravity-y').innerHTML = e.accelerationIncludingGravity.y+"m/s²";
-  document.getElementById('acceleration-gravity-z').innerHTML = e.accelerationIncludingGravity.z+"m/s²";
+  document.getElementById('acceleration-gravity-x').innerHTML = e.accelerationIncludingGravity.x.toFixed(2)+"m/s²";
+  document.getElementById('acceleration-gravity-y').innerHTML = e.accelerationIncludingGravity.y.toFixed(2)+"m/s²";
+  document.getElementById('acceleration-gravity-z').innerHTML = e.accelerationIncludingGravity.z.toFixed(2)+"m/s²";
 
-  document.getElementById('rotation-alpha').innerHTML = e.rotationRate.alpha+"°/s";
-  document.getElementById('rotation-beta').innerHTML = e.rotationRate.beta+"°/s";
-  document.getElementById('rotation-gamma').innerHTML = e.rotationRate.gamma+"°/s";
+  document.getElementById('rotation-alpha').innerHTML = e.rotationRate.alpha.toFixed(2)+"°/s";
+  document.getElementById('rotation-beta').innerHTML = e.rotationRate.beta.toFixed(2)+"°/s";
+  document.getElementById('rotation-gamma').innerHTML = e.rotationRate.gamma.toFixed(2)+"°/s";
 });
