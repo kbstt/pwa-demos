29d6948a7d44cf035be360926dcf4f2f8c9f94bb
kbstt <80958930+kbstt@users.noreply.github.com>
2023-09-06 00:59:35 -0500

    Update device-motion-event-pwa.js

--- device-motion-event-pwa.js
+++ device-motion-event-pwa.js
@@ -1,13 +1,13 @@
 window.addEventListener("devicemotion", function(e){
-  document.getElementById('acceleration-x').innerHTML = e.acceleration.x.toFixed(1)+"m/s²";
-  document.getElementById('acceleration-y').innerHTML = e.acceleration.y.toFixed(1)+"m/s²";
-  document.getElementById('acceleration-z').innerHTML = e.acceleration.z.toFixed(1)+"m/s²";
+  document.getElementById('acceleration-x').innerHTML = e.acceleration.x.toFixed(2)+"m/s²";
+  document.getElementById('acceleration-y').innerHTML = e.acceleration.y.toFixed(2)+"m/s²";
+  document.getElementById('acceleration-z').innerHTML = e.acceleration.z.toFixed(2)+"m/s²";
 
-  document.getElementById('acceleration-gravity-x').innerHTML = e.accelerationIncludingGravity.x.toFixed(1)+"m/s²";
-  document.getElementById('acceleration-gravity-y').innerHTML = e.accelerationIncludingGravity.y.toFixed(1)+"m/s²";
-  document.getElementById('acceleration-gravity-z').innerHTML = e.accelerationIncludingGravity.z.toFixed(1)+"m/s²";
+  document.getElementById('acceleration-gravity-x').innerHTML = e.accelerationIncludingGravity.x.toFixed(2)+"m/s²";
+  document.getElementById('acceleration-gravity-y').innerHTML = e.accelerationIncludingGravity.y.toFixed(2)+"m/s²";
+  document.getElementById('acceleration-gravity-z').innerHTML = e.accelerationIncludingGravity.z.toFixed(2)+"m/s²";
 
-  document.getElementById('rotation-alpha').innerHTML = e.rotationRate.alpha.toFixed(1)+"°/s";
-  document.getElementById('rotation-beta').innerHTML = e.rotationRate.beta.toFixed(1)+"°/s";
-  document.getElementById('rotation-gamma').innerHTML = e.rotationRate.gamma.toFixed(1)+"°/s";
+  document.getElementById('rotation-alpha').innerHTML = e.rotationRate.alpha.toFixed(2)+"°/s";
+  document.getElementById('rotation-beta').innerHTML = e.rotationRate.beta.toFixed(2)+"°/s";
+  document.getElementById('rotation-gamma').innerHTML = e.rotationRate.gamma.toFixed(2)+"°/s";
 });
