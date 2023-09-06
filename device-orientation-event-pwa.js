cc040d58a6631d474381df3677f4e71f16269aba
kbstt <80958930+kbstt@users.noreply.github.com>
2023-09-06 00:11:42 -0500

    Update device-orientation-event-pwa.js

--- device-orientation-event-pwa.js
+++ device-orientation-event-pwa.js
@@ -1,6 +1,6 @@
 window.addEventListener("deviceorientation", function(e){
-  document.getElementById('alpha').innerHTML = e.alpha; //angle of motion around the Z axis
-  document.getElementById('beta').innerHTML = e.beta; //angle of motion around the X axis
-  document.getElementById('gamma').innerHTML = e.gamme; //angle of motion around the Y axis
+  document.getElementById('alpha').innerHTML = e.alpha+"°"; //angle of motion around the Z axis
+  document.getElementById('beta').innerHTML = e.beta+"°"; //angle of motion around the X axis
+  document.getElementById('gamma').innerHTML = e.gamme+"°"; //angle of motion around the Y axis
   document.getElementById('orientation').innerHTML = Math.abs(beta) > Math.abs(gamma) ? "landscape" : "portrait";
 });
