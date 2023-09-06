f485b24acb09f05e3b317e1e226d1eb4ebce877f
kbstt <80958930+kbstt@users.noreply.github.com>
2023-09-06 01:20:32 -0500

    Update device-orientation-event-pwa.js

--- device-orientation-event-pwa.js
+++ device-orientation-event-pwa.js
@@ -1,6 +1,17 @@
-window.addEventListener("deviceorientation", function(e){
-  document.getElementById('alpha').innerHTML = e.alpha.toFixed(1)+"°"; //angle of motion around the Z axis
-  document.getElementById('beta').innerHTML = e.beta.toFixed(1)+"°"; //angle of motion around the X axis
-  document.getElementById('gamma').innerHTML = e.gamma.toFixed(1)+"°"; //angle of motion around the Y axis
-  document.getElementById('orientation').innerHTML = Math.abs(e.beta) > Math.abs(e.gamma) ? "portrait" : "landscape";
-});
+async function getOrientation(){
+  if (!window.DeviceOrientationEvent || !window.DeviceOrientationEvent.requestPermission){
+    return alert("Your current device does not have access to the DeviceOrientation event");
+  }
+
+  let permission = await window.DeviceOrientationEvent.requestPermission();
+  if (permission !== "granted"){
+    return alert("You must grant access to the device's sensor for this demo");
+  }
+  
+  window.addEventListener("deviceorientation", function(e){
+    document.getElementById('alpha').innerHTML = e.alpha.toFixed(1)+"°"; //angle of motion around the Z axis
+    document.getElementById('beta').innerHTML = e.beta.toFixed(1)+"°"; //angle of motion around the X axis
+    document.getElementById('gamma').innerHTML = e.gamma.toFixed(1)+"°"; //angle of motion around the Y axis
+    document.getElementById('orientation').innerHTML = Math.abs(e.beta) > Math.abs(e.gamma) ? "portrait" : "landscape";
+  });
+}
