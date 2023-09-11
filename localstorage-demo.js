9008c1d71b6a5e35f8c8804ced045ce88be8cc3a
kbstt <80958930+kbstt@users.noreply.github.com>
2023-09-11 00:32:22 -0500

    Update localstorage-demo.js

--- localstorage-demo.js
+++ localstorage-demo.js
@@ -1,4 +1,5 @@
 function renderFilePreview(base64Data){
+  console.log(base64Data);
   let blob = new Blob([base64Data], { type: "image/png" });
   let previewContainer = document.getElementById("file-preview-container");
   previewContainer.innerHTML = "";
