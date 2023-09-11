2ff2a459de99053eee7e08b13653b774d7ea3a2c
kbstt <80958930+kbstt@users.noreply.github.com>
2023-09-11 00:42:12 -0500

    Update localstorage-demo.js

--- localstorage-demo.js
+++ localstorage-demo.js
@@ -1,5 +1,4 @@
 function renderFilePreview(base64Data){
-  console.log(base64Data);
   let previewContainer = document.getElementById("file-preview-container");
   previewContainer.innerHTML = "";
   let imgPreview = document.createElement("img");
@@ -8,6 +7,7 @@ function renderFilePreview(base64Data){
 }
 
 function processFileSelection(event){
+  //we save the file selected in base64 format
   let file = event.target.files[0];
   if (!file) {return alert('No file selected');}
   let reader = new FileReader();
@@ -20,7 +20,11 @@ function processFileSelection(event){
 }
 
 async function initializePage(){
+  //we add the file picker functionality to the input file we have on the page
   document.getElementById('upload-file').addEventListener('change', processFileSelection);
+
+  //if we have a file uploaded from a previous session, 
+  //we get it from localStorage and render it on the page
   let base64Data = localStorage.getItem('uploadedFile');
   if (!base64Data){return;}
   renderFilePreview(base64Data);
