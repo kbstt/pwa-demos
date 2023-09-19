4cd3c75360aab417d3112b61dc650d62c1b54653
kbstt <80958930+kbstt@users.noreply.github.com>
2023-09-19 00:21:53 -0500

    Update biometric-authentication-pwa-demo.js

--- biometric-authentication-pwa-demo.js
+++ biometric-authentication-pwa-demo.js
@@ -16,7 +16,7 @@ async function createPasskey(){
   
   let credentials = await navigator.credentials.create({
       publicKey: {
-        challenge: generateRandomChallenge()),
+        challenge: generateRandomChallenge(),
         rp: { name: "Progressier", id: window.location.hostname },
         //here you'll want to pass the user's info
         user: { id: new Uint8Array(16), name: "johndoe@progressier.com", displayName: "John Doe"},
@@ -24,6 +24,10 @@ async function createPasskey(){
             { type: "public-key", alg: -7 },
             { type: "public-key", alg: -257 }
         ],
+        timeout: 60000,
+        authenticatorSelection: {residentKey: "preferred", requireResidentKey: false, userVerification: "preferred"},
+        attestation: "none",
+        extensions": { credProps: true}
       }
   });
   //in a real app, you'll store the credentials against the user's profile in your DB
