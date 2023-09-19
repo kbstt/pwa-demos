9f84cfde3433ebb8a11c356fe3701b9ab72d3fcc
kbstt <80958930+kbstt@users.noreply.github.com>
2023-09-18 23:38:14 -0500

    Update biometric-authentication-pwa-demo.js

--- biometric-authentication-pwa-demo.js
+++ biometric-authentication-pwa-demo.js
@@ -1,28 +1,19 @@
+const randomUserId = function(){
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+
+    for (let i = 0; i < 8; i++) {
+        result += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+
+    return result;
+}();
+  
 const signupSampleCredentials = {
   publicKey: {
-    rp: {
-      name: "Progressier",
-      id: window.location.hostname,
-    },
-    user: {
-      id: new Uint8Array(16),
-      name: "pwa-demo@example.com",
-      displayName: "PWA Demo",
-    },
-    pubKeyCredParams: [
-      {
-        type: "public-key",
-        alg: -7,
-      },
-    ],
-    attestation: "direct",
-    timeout: 60000,
-    challenge: new Uint8Array([
-      // must be a cryptographically random number sent from a server
-      0x8c, 0x0a, 0x26, 0xff, 0x22, 0x91, 0xc1, 0xe9, 0xb9, 0x4e, 0x2e, 0x17, 0x1a,
-      0x98, 0x6a, 0x73, 0x71, 0x9d, 0x43, 0x48, 0xd5, 0xa7, 0x6a, 0x15, 0x7e, 0x38,
-      0x94, 0x52, 0x77, 0x97, 0x0f, 0xef,
-    ]).buffer,
+    rp: { name: "Progressier", id: window.location.hostname },
+    user: { id: new Uint8Array(16), name: randomUserId, displayName: randomUserId},
+    pubKeyCredParams: [{ type: "public-key", alg: -7 }],
   },
 };
 
