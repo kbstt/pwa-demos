4321cebe8f8c9b15ae70ab020dd089d3ffe91db5
kbstt <80958930+kbstt@users.noreply.github.com>
2023-09-19 00:09:11 -0500

    Update biometric-authentication-pwa-demo.js

--- biometric-authentication-pwa-demo.js
+++ biometric-authentication-pwa-demo.js
@@ -1,3 +1,12 @@
+//the challenge is a crucial part of the authentication process, and is used to mitigate "replay attacks" and allow server-side authentication
+//in a real app, you'll want to generate the challenge server-side and maintain a session or temporary record of this challenge in your DB
+function generateRandomChallenge() {
+    const length = 32;
+    const randomValues = new Uint8Array(length);
+    window.crypto.getRandomValues(randomValues);
+    return randomValues;
+}
+
 async function createPasskey(){
   if (!navigator.credentials || !navigator.credentials.create || !navigator.credentials.get){
     return alert("Your browser does not support the Web Authentication API");
@@ -5,7 +14,7 @@ async function createPasskey(){
   
   let credentials = await navigator.credentials.create({
       publicKey: {
-        challenge: new Uint8Array([43, 55, 91, 123, 09, 181, 181, 78]),
+        challenge: generateRandomChallenge()),
         rp: { name: "Progressier", id: window.location.hostname },
         user: { id: new Uint8Array(16), name: "demo@progressier.com", displayName: "demo@progressier.com"},
         pubKeyCredParams: [
@@ -15,14 +24,29 @@ async function createPasskey(){
       }
   });
   //in a real app, you'll store the credentials in your database against the user's profile
+  //here we'll just save it in a global variable
   window.currentPasskey = credentials;
   console.log(credentials);
+
+  //we update our demo buttons
   document.getElementById('authenticate-btn').innerHTML = "Authenticated";
   document.getElementById('authenticate-btn').classList.add('disabled'); 
   document.getElementById('verify-btn').classList.remove('disabled');
 }
 
 async function verifyPasskey(){
-  
-
+  try {
+    await navigator.credentials.get({
+        publicKey: {
+          challenge: generateRandomChallenge(),
+          allowCredentials: [{
+              type: "public-key",
+              id: window.currentPasskey.id
+          }]
+        }
+    });
+  }
+  catch(err){
+    alert(err);
+  }
 }
