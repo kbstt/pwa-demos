7c7ddb9288ad5d07b031a08aae8a28dd56aa37eb
kbstt <80958930+kbstt@users.noreply.github.com>
2023-09-18 23:57:29 -0500

    Update biometric-authentication-pwa-demo.js

--- biometric-authentication-pwa-demo.js
+++ biometric-authentication-pwa-demo.js
@@ -1,14 +1,3 @@
-const randomUserId = function(){
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    let result = '';
-
-    for (let i = 0; i < 8; i++) {
-        result += characters.charAt(Math.floor(Math.random() * characters.length));
-    }
-
-    return result;
-}();
-
 async function createPasskey(){
   if (!navigator.credentials || !navigator.credentials.create || !navigator.credentials.get){
     return alert("Your browser does not support the Web Authentication API");
@@ -18,14 +7,19 @@ async function createPasskey(){
       publicKey: {
         challenge: new Uint8Array([43, 55, 91, 123, 09, 181, 181, 78]),
         rp: { name: "Progressier", id: window.location.hostname },
-        user: { id: new Uint8Array(16), name: randomUserId, displayName: randomUserId},
+        user: { id: new Uint8Array(16), name: "demo@progressier.com", displayName: "demo@progressier.com"},
         pubKeyCredParams: [
             { type: "public-key", alg: -7 },
             { type: "public-key", alg: -257 }
         ],
       }
   });
+  //in a real app, you'll store the credentials in your database against the user's profile
+  window.currentPasskey = credentials;
   console.log(credentials);
+  document.getElementById('authenticate-btn').innerHTML = "Authenticated";
+  document.getElementById('authenticate-btn').classList.add('disabled'); 
+  document.getElementById('verify-btn').classList.remove('disabled');
 }
 
 async function verifyPasskey(){
