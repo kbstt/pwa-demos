6ec9b4a6a8116cc8eeda0208524797ae79530c64
kbstt <80958930+kbstt@users.noreply.github.com>
2023-09-18 23:39:53 -0500

    Update biometric-authentication-pwa-demo.js

--- biometric-authentication-pwa-demo.js
+++ biometric-authentication-pwa-demo.js
@@ -8,42 +8,20 @@ const randomUserId = function(){
 
     return result;
 }();
-  
-const signupSampleCredentials = {
-  publicKey: {
-    rp: { name: "Progressier", id: window.location.hostname },
-    user: { id: new Uint8Array(16), name: randomUserId, displayName: randomUserId},
-    pubKeyCredParams: [{ type: "public-key", alg: -7 }],
-  },
-};
-
-// sample arguments for login
-const loginSampleCredentials = {
-  publicKey: {
-    timeout: 60000,
-    challenge: new Uint8Array([
-      // must be a cryptographically random number sent from a server
-      0x79, 0x50, 0x68, 0x71, 0xda, 0xee, 0xee, 0xb9, 0x94, 0xc3, 0xc2, 0x15, 0x67,
-      0x65, 0x26, 0x22, 0xe3, 0xf3, 0xab, 0x3b, 0x78, 0x2e, 0xd5, 0x6f, 0x81, 0x26,
-      0xe2, 0xa6, 0x01, 0x7d, 0x74, 0x50,
-    ]).buffer,
-  },
-};
 
 async function createPasskey(){
   if (!navigator.credentials || !navigator.credentials.create || !navigator.credentials.get){
     return alert("Your browser does not support the Web Authentication API");
   }
   
-  let credentials = await navigator.credentials.create(signupSampleCredentials);
-  let idList = [{
-    id: credential.rawId,
-    transports: ["usb", "nfc", "ble"],
-    type: "public-key",
-  }];
-  
-  loginSampleCredentials.publicKey.allowCredentials = idList;
-  return navigator.credentials.get(loginSampleCredentials);
+  let credentials = await navigator.credentials.create({
+      publicKey: {
+        rp: { name: "Progressier", id: window.location.hostname },
+        user: { id: new Uint8Array(16), name: randomUserId, displayName: randomUserId},
+        pubKeyCredParams: [{ type: "public-key", alg: -7 }],
+      }
+  });
+  console.log(credentials);
 }
 
 async function verifyPasskey(){
