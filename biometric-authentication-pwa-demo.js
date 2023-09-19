b74508b7572a13d31bd97d60058863cdc8e17697
kbstt <80958930+kbstt@users.noreply.github.com>
2023-09-18 23:24:12 -0500

    Create biometric-authentication-pwa-demo.js

---
+++ biometric-authentication-pwa-demo.js
@@ -0,0 +1,60 @@
+const signupSampleCredentials = {
+  publicKey: {
+    rp: {
+      name: "Progressier",
+    },
+    user: {
+      id: new Uint8Array(16),
+      name: "pwa-demo@example.com",
+      displayName: "PWA Demo",
+    },
+    pubKeyCredParams: [
+      {
+        type: "public-key",
+        alg: -7,
+      },
+    ],
+    attestation: "direct",
+    timeout: 60000,
+    challenge: new Uint8Array([
+      // must be a cryptographically random number sent from a server
+      0x8c, 0x0a, 0x26, 0xff, 0x22, 0x91, 0xc1, 0xe9, 0xb9, 0x4e, 0x2e, 0x17, 0x1a,
+      0x98, 0x6a, 0x73, 0x71, 0x9d, 0x43, 0x48, 0xd5, 0xa7, 0x6a, 0x15, 0x7e, 0x38,
+      0x94, 0x52, 0x77, 0x97, 0x0f, 0xef,
+    ]).buffer,
+  },
+};
+
+// sample arguments for login
+const loginSampleCredentials = {
+  publicKey: {
+    timeout: 60000,
+    challenge: new Uint8Array([
+      // must be a cryptographically random number sent from a server
+      0x79, 0x50, 0x68, 0x71, 0xda, 0xee, 0xee, 0xb9, 0x94, 0xc3, 0xc2, 0x15, 0x67,
+      0x65, 0x26, 0x22, 0xe3, 0xf3, 0xab, 0x3b, 0x78, 0x2e, 0xd5, 0x6f, 0x81, 0x26,
+      0xe2, 0xa6, 0x01, 0x7d, 0x74, 0x50,
+    ]).buffer,
+  },
+};
+
+async function createPasskey(){
+  if (!navigator.credentials || !navigator.credentials.create || !navigator.credentials.get){
+    return alert("Your browser does not support the Web Authentication API");
+  }
+  
+  let credentials = await navigator.credentials.create(signupSampleCredentials);
+  let idList = [{
+    id: credential.rawId,
+    transports: ["usb", "nfc", "ble"],
+    type: "public-key",
+  }];
+  
+  loginSampleCredentials.publicKey.allowCredentials = idList;
+  return navigator.credentials.get(loginSampleCredentials);
+}
+
+async function verifyPasskey(){
+  
+
+}
