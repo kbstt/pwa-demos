29a3138bb36b200f466b59e539b800b8f05b3a49
kbstt <80958930+kbstt@users.noreply.github.com>
2023-09-19 00:15:02 -0500

    Update biometric-authentication-pwa-demo.js

--- biometric-authentication-pwa-demo.js
+++ biometric-authentication-pwa-demo.js
@@ -1,5 +1,7 @@
-//the challenge is a crucial part of the authentication process, and is used to mitigate "replay attacks" and allow server-side authentication
-//in a real app, you'll want to generate the challenge server-side and maintain a session or temporary record of this challenge in your DB
+//the challenge is a crucial part of the authentication process, 
+//and is used to mitigate "replay attacks" and allow server-side authentication
+//in a real app, you'll want to generate the challenge server-side and 
+//maintain a session or temporary record of this challenge in your DB
 function generateRandomChallenge() {
     const length = 32;
     const randomValues = new Uint8Array(length);
@@ -16,27 +18,31 @@ async function createPasskey(){
       publicKey: {
         challenge: generateRandomChallenge()),
         rp: { name: "Progressier", id: window.location.hostname },
-        user: { id: new Uint8Array(16), name: "demo@progressier.com", displayName: "demo@progressier.com"},
+        //here you'll want to pass the user's info
+        user: { id: new Uint8Array(16), name: "johndoe@progressier.com", displayName: "John Doe"},
         pubKeyCredParams: [
             { type: "public-key", alg: -7 },
             { type: "public-key", alg: -257 }
         ],
       }
   });
-  //in a real app, you'll store the credentials in your database against the user's profile
+  //in a real app, you'll store the credentials against the user's profile in your DB
   //here we'll just save it in a global variable
   window.currentPasskey = credentials;
   console.log(credentials);
 
   //we update our demo buttons
-  document.getElementById('authenticate-btn').innerHTML = "Authenticated";
-  document.getElementById('authenticate-btn').classList.add('disabled'); 
-  document.getElementById('verify-btn').classList.remove('disabled');
+  document.getElementById("authenticate-btn").innerHTML = "Authenticated";
+  document.getElementById("authenticate-btn").classList.add("disabled"); 
+  document.getElementById("verify-btn").classList.remove("disabled");
 }
 
 async function verifyPasskey(){
   try {
-    await navigator.credentials.get({
+    //to verify a user's credentials, we simply pass the 
+    //unique ID of the passkey we saved against the user profile
+    //in this example, we simply stored it in a global variable
+    let credentials = await navigator.credentials.get({
         publicKey: {
           challenge: generateRandomChallenge(),
           allowCredentials: [{
@@ -45,6 +51,8 @@ async function verifyPasskey(){
           }]
         }
     });
+    console.log(credentials);  
+    alert("Passkey match");
   }
   catch(err){
     alert(err);
