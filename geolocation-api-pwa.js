75ee824a92436a67d5f4024b50c9233103e9feae
kbstt <80958930+kbstt@users.noreply.github.com>
2023-11-01 06:10:19 +0800

    Update geolocation-api-pwa.js

--- geolocation-api-pwa.js
+++ geolocation-api-pwa.js
@@ -1,5 +1,19 @@
-function markLocation(position){
-  updateMap(position.coords.latitude, position.coords.longitude);
+function updateMap(position){
+    // These are the coordinates returned by the Geolocation API
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+  
+    // Recenter our map around the coordinates
+    window.demoMap.setView([latitude, longitude], 13);
+  
+    // Add a marker at the specified coordinates
+    var marker = L.marker([latitude, longitude]).addTo(window.demoMap);
+
+    // Add a popup to the marker
+    marker.bindPopup("Hello, this is your location!");
+
+    // For demo purposes only, display the latitude and longitude to the user
+    alert("Latitude:"+latitude+", Longitude:"+longitude);
 }
 
 function triggerAlert(err){
@@ -21,20 +35,5 @@ function createMap(){
   window.demoMap = L.map("map").setView([48.8584, 2.2945], 2);
   
   // Add a tile layer from OpenStreetMap's default tile server
-  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png?{foo}', {foo: 'bar', attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'}).addTo(window.demoMap);
-  
-}
-
-function updateMap(latitude, longitude){
-    // Recenter our map around the coordinates
-    window.demoMap.setView([latitude, longitude], 13);
-  
-    // Add a marker at the specified coordinates
-    var marker = L.marker([latitude, longitude]).addTo(window.demoMap);
-
-    // Add a popup to the marker
-    marker.bindPopup("Hello, this is your location!");
-
-    // For demo purposes only, display the latitude and longitude to the user
-    alert("Latitude:"+latitude+", Longitude:"+longitude);
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png?{foo}', {foo: 'bar', attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'}).addTo(window.demoMap); 
 }
