43f5f9bccaa71adc2c88df914bd6410da6056c56
kbstt <80958930+kbstt@users.noreply.github.com>
2024-04-15 09:39:43 +0800

    Update speech-recognition-api-pwa.js

--- speech-recognition-api-pwa.js
+++ speech-recognition-api-pwa.js
@@ -23,3 +23,15 @@ function toggleSpeechRecognition(){
     window.transcriptionInProgress.start();
   }
 }
+
+function createSelectorForLanguageCodes(){
+  //this is not an exhaustive list. Check https://en.wikipedia.org/wiki/IETF_language_tag for all available codes.
+  let commonLanguageLocales = ["ar-SA", "bg-BG", "bn-BD", "cs-CZ", "da-DK", "de-DE", "el-GR", "en-AU", "en-CA", "en-GB", "en-IE", "en-NZ", "en-US", "en-ZA", "es-AR", "es-BO", "es-CL", "es-CO", "es-CR", "es-CU", "es-DO", "es-EC", "es-ES", "es-GT", "es-HN", "es-MX", "es-NI", "es-PA", "es-PE", "es-PR", "es-PY", "es-SV", "es-UY", "es-VE" "et-EE", "fa-IR", "fi-FI", "fr-CA", "fr-CH", "fr-BE", "fr-FR", "gu-IN", "hi-IN", "hr-HR", "hu-HU", "id-ID", "it-IT", "ja-JP", "jv-ID", "km-KH", "kn-IN", "ko-KR", "lt-LT", "lv-LV", "ml-IN", "mr-IN", "mt-MT", "my-MM", "nl-NL", "no-NO", "or-IN", "pa-IN", "pl-PL", "pt-BR", "ro-RO", "ru-RU", "sk-SK", "sl-SI", "sr-RS", "sv-SE", "ta-IN", "te-IN", "th-TH", "tr-TR", "uk-UA", "ur-PK", "vi-VN", "zh-CN", "zh-TW"];
+  let container = document.getElementById("language-selector");
+  commonLanguageLocales.forEach(function(code){
+     let opt = document.createElement("option");
+     opt.innerHTML = code;
+     opt.setAttribute("value", code);
+     container.appendChild(opt);
+  });
+},
