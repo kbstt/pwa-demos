a5eda942a7d81bdf01730d03575649f230e9f0b5
kbstt <80958930+kbstt@users.noreply.github.com>
2025-10-17 10:14:17 +0800

    Update pwa-vibration-api.js

--- pwa-vibration-api.js
+++ pwa-vibration-api.js
@@ -16,12 +16,12 @@ function vibrationPattern(index){
   a label to an invisible switch input and toggling it programmatically. 
   */
   else { 
-  let el = document.createElement('div');
-  let id = Math.random().toString(36).slice(2);
-  el.innerHTML = `<input type="checkbox" id="`+id+`" switch /><label for="`+id+`"></label>`;
-  el.setAttribute("style", "display:none !important;opacity:0 !important;visibility:hidden !important;");
-  document.querySelector('body').appendChild(el);
-  el.querySelector('label').click();
-  setTimeout(function(){ el.remove(); }, 1500);
+    let el = document.createElement('div');
+    let id = Math.random().toString(36).slice(2);
+    el.innerHTML = `<input type="checkbox" id="`+id+`" switch /><label for="`+id+`"></label>`;
+    el.setAttribute("style", "display:none !important;opacity:0 !important;visibility:hidden !important;");
+    document.querySelector('body').appendChild(el);
+    el.querySelector('label').click();
+    setTimeout(function(){ el.remove(); }, 1500);
   }
 }
