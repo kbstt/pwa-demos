c9a1773ab8a895b5e445024e462cc38d744a0b0b
kbstt <80958930+kbstt@users.noreply.github.com>
2025-11-10 12:19:07 +0800

    Update detect-faces-with-shape-detection-api-in-pwa.js

--- detect-faces-with-shape-detection-api-in-pwa.js
+++ detect-faces-with-shape-detection-api-in-pwa.js
@@ -18,13 +18,14 @@ async function drawCurrentFrameOnCanvasAndDetectFaces(){
   ctx.strokeStyle = 'lime';
   ctx.fillStyle = 'red';
   faces.forEach(function(face){
-    console.log(face);
     const { boundingBox, landmarks } = face;
-    ctx.strokeRect(boundingBox.x, boundingBox.y, boundingBox.width, boundingBox.height);
+    const scaleX = window.faceDetectionCanvas.width / window.faceDetectionVideo.videoWidth;
+    const scaleY = window.faceDetectionCanvas.height / window.faceDetectionVideo.videoHeight;
+     ctx.strokeRect(boundingBox.x * scaleX, boundingBox.y * scaleY, boundingBox.width * scaleX, boundingBox.height * scaleY);
     const points = (landmarks ?? []).flatMap(landmark => landmark.locations ?? []);
     points.forEach(point => {
       ctx.beginPath();
-      ctx.arc(point.x, point.y, 3, 0, 2 * Math.PI);
+      ctx.arc(point.x * scaleX, point.y * scaleY, 3, 0, 2 * Math.PI);
       ctx.fill();
     });
   });
