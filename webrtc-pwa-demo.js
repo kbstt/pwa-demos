c3f54db1d3033dc0db0c41e44269c01ab1cf341b
kbstt <80958930+kbstt@users.noreply.github.com>
2025-11-10 15:05:53 +0800

    Create webrtc-pwa-demo.js

---
+++ webrtc-pwa-demo.js
@@ -0,0 +1,29 @@
+let pc = new RTCPeerConnection();
+let video = document.getElementById('video');
+
+pc.ontrack = e => video.srcObject = e.streams[0];
+
+async function start() {
+  let stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+  stream.getTracks().forEach(t => pc.addTrack(t, stream));
+  video.srcObject = stream;
+}
+
+async function createOffer() {
+  let offer = await pc.createOffer();
+  await pc.setLocalDescription(offer);
+  document.getElementById('local').value = JSON.stringify(offer);
+}
+
+async function createAnswer() {
+  let offer = JSON.parse(document.getElementById('remote').value);
+  await pc.setRemoteDescription(offer);
+  let answer = await pc.createAnswer();
+  await pc.setLocalDescription(answer);
+  document.getElementById('local').value = JSON.stringify(answer);
+}
+
+async function setRemote() {
+  let answer = JSON.parse(document.getElementById('remote').value);
+  await pc.setRemoteDescription(answer);
+}
