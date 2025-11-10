533f80cea3ce4016975bea3e9e21d6aff0aee1d7
kbstt <80958930+kbstt@users.noreply.github.com>
2025-11-10 15:23:19 +0800

    Update webrtc-pwa-demo.js

--- webrtc-pwa-demo.js
+++ webrtc-pwa-demo.js
@@ -1,28 +1,26 @@
-window.rtcConnectionInstance = new RTCPeerConnection();
-window.rtcConnectionInstance.ontrack = e => video.srcObject = e.streams[0];
+window.p2pConnection = new RTCPeerConnection();
+window.rtcVideoElement = document.getElementById('rtc-demo-video');
+window.p2pConnection.ontrack = e => window.rtcVideoElement.srcObject = e.streams[0];
+window.p2pConnection.onicecandidate = e => {
+  if (e.candidate) {console.log('ICE candidate:', e.candidate);}
+};
 
 async function startStreaming() {
-  let video = document.getElementById('rtc-demo-video');
   let stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
-  stream.getTracks().forEach(t => window.rtcConnectionInstance.addTrack(t, stream));
-  video.srcObject = stream;
+  window.rtcVideoElement.srcObject = stream;
+  stream.getTracks().forEach(track => window.p2pConnection.addTrack(track, stream));
+  let offer = await window.p2pConnection.createOffer();
+  await window.p2pConnection.setLocalDescription(offer);
+  document.getElementById('local').value = JSON.stringify(window.p2pConnection.localDescription);
 }
 
-async function createOffer() {
-  let offer = await window.rtcConnectionInstance.createOffer();
-  await window.rtcConnectionInstance.setLocalDescription(offer);
-  document.getElementById('local').value = JSON.stringify(offer);
-}
-
-async function createAnswer() {
+async function answerAndConnect() {
+  let stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+  window.rtcVideoElement.srcObject = stream;
+  stream.getTracks().forEach(track => window.p2pConnection.addTrack(track, stream));
   let offer = JSON.parse(document.getElementById('remote').value);
-  await window.rtcConnectionInstance.setRemoteDescription(offer);
-  let answer = await window.rtcConnectionInstance.createAnswer();
-  await window.rtcConnectionInstance.setLocalDescription(answer);
-  document.getElementById('local').value = JSON.stringify(answer);
-}
-
-async function setRemote() {
-  let answer = JSON.parse(document.getElementById('remote').value);
-  await window.rtcConnectionInstance.setRemoteDescription(answer);
+  await window.p2pConnection.setRemoteDescription(offer);
+  let answer = await window.p2pConnection.createAnswer();
+  await window.p2pConnection.setLocalDescription(answer);
+  document.getElementById('local').value = JSON.stringify(window.p2pConnection.localDescription);
 }
