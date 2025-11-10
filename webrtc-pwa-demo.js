e055ce6e77fed1194dc5bf9b942f11eab178efcf
kbstt <80958930+kbstt@users.noreply.github.com>
2025-11-10 15:41:20 +0800

    Update webrtc-pwa-demo.js

--- webrtc-pwa-demo.js
+++ webrtc-pwa-demo.js
@@ -19,11 +19,10 @@ async function startStreaming() {
 
 async function answerAndConnect() {
   initP2P();
-  let stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
-  window.rtcVideoElement.srcObject = stream;
-  stream.getTracks().forEach(track => window.p2pConnection.addTrack(track, stream));
   let offer = JSON.parse(document.getElementById('webrtc-offer-answer').value);
   await window.p2pConnection.setRemoteDescription(offer);
+  window.p2pConnection.addTransceiver('video', { direction: 'recvonly' });
+  window.p2pConnection.addTransceiver('audio', { direction: 'recvonly' });
   let answer = await window.p2pConnection.createAnswer();
   await window.p2pConnection.setLocalDescription(answer);
   document.getElementById('webrtc-offer-answer').value = JSON.stringify(window.p2pConnection.localDescription);
