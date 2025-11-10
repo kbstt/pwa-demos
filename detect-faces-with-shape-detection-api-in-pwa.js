6179628273d40dbf2bcd002ad4ff3bef22887fbe
kbstt <80958930+kbstt@users.noreply.github.com>
2025-11-10 12:30:56 +0800

    Update detect-faces-with-shape-detection-api-in-pwa.js

--- detect-faces-with-shape-detection-api-in-pwa.js
+++ detect-faces-with-shape-detection-api-in-pwa.js
@@ -12,23 +12,24 @@ async function startFaceDetection() {
 }
 
 async function drawCurrentFrameOnCanvasAndDetectFaces(){
+  //we draw the video stream on a canvas
   let ctx0 = window.faceDetectionCanvas.getContext('2d');
   ctx0.drawImage(window.faceDetectionVideo, 0, 0, window.faceDetectionCanvas.width, window.faceDetectionCanvas.height);
   let faces = await window.faceDetectorInstance.detect(window.faceDetectionCanvas);
 
+  //we populate the features on a different canvas
   let ctx = window.faceDetectionFeatureCanvas.getContext('2d');
+  ctx.clearRect(0, 0, window.faceDetectionFeatureCanvas.width, window.faceDetectionFeatureCanvas.height);
   ctx.lineWidth = 2;
   ctx.strokeStyle = 'lime';
   ctx.fillStyle = 'red';
   faces.forEach(function(face){
     const { boundingBox, landmarks } = face;
-    const scaleX = window.faceDetectionCanvas.width / window.faceDetectionVideo.videoWidth;
-    const scaleY = window.faceDetectionCanvas.height / window.faceDetectionVideo.videoHeight;
-    ctx.strokeRect(boundingBox.x * scaleX, boundingBox.y * scaleY, boundingBox.width * scaleX, boundingBox.height * scaleY);
+    ctx.strokeRect(boundingBox.x, boundingBox.y, boundingBox.width, boundingBox.height);
     const points = (landmarks ?? []).flatMap(landmark => landmark.locations ?? []);
     points.forEach(point => {
       ctx.beginPath();
-      ctx.arc(point.x * scaleX, point.y * scaleY, 3, 0, 2 * Math.PI);
+      ctx.arc(point.x, point.y, 3, 0, 2 * Math.PI);
       ctx.fill();
     });
   });
