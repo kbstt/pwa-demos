e495f3654b1cc8a99f944c9cd7c63bd071b45bc1
kbstt <80958930+kbstt@users.noreply.github.com>
2025-11-10 14:26:04 +0800

    Create text-detection-api-in-pwa.js

---
+++ text-detection-api-in-pwa.js
@@ -0,0 +1,20 @@
+function uploadImageToDetectText(){
+   if (!window.TextDetector)) {
+        alert('TextDetector API not supported in this browser.');
+        return;
+    }
+    let file = await new Promise(resolve => {
+        let input = document.createElement('input');
+        input.type = 'file';
+        input.accept = 'image/*';
+        input.onchange = () => resolve(input.files);
+        input.click();
+    });
+    if (!file) {return};
+    const img = new Image();
+    img.src = URL.createObjectURL(file);
+    await img.decode();
+    const detector = new TextDetector();
+    const results = await detector.detect(img);
+    results.forEach(result => console.log(result.rawValue));
+}
