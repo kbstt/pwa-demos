d0bf9027d669cba0ec80e1057fc5c2fa0740e2a9
kbstt <80958930+kbstt@users.noreply.github.com>
2025-11-10 11:31:21 +0800

    Create detect-faces-with-shape-detection-api-in-pwa.js

---
+++ detect-faces-with-shape-detection-api-in-pwa.js
@@ -0,0 +1,22 @@
+function startFaceDetection() {
+  if (!window.FaceDetector){
+    alert("The Face Detector API is not available in your current browser");
+    return;
+  }
+  
+  let video = document.getElementById('face-detection-video');
+  let stream = await navigator.mediaDevices.getUserMedia({ video: {facingMode: "environment"}});
+  video.srcObject = stream;
+  video.play();
+  video.addEventListener('loadedmetadata', async function(){
+      let canvas = document.getElementById('face-detection-canvas');
+      let context = canvas.getContext('2d');
+
+     let detectFace = async function(){
+          context.drawImage(video, 0, 0, canvas.width, canvas.height);
+          new FaceDetector().detect(canvas);
+          requestAnimationFrame(detectFace);
+     };       
+     detectFace();
+  });
+}
