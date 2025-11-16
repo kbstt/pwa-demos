93237bf622c138a46124e72d0c090f5c258249d2
kbstt <80958930+kbstt@users.noreply.github.com>
2025-11-17 07:15:03 +0800

    Update permissions-api-demo.js

--- permissions-api-demo.js
+++ permissions-api-demo.js
@@ -1,16 +1,13 @@
 async function printPermission(permissionName, descriptor) {
     try {
       let permission = await navigator.permissions.query(Object.assign({name: permissionName}, descriptor));
-      let result = document.createElement("tr");
-      result.innerHTML = "<td>"+permissionName+"</td><td>"+permission.state+"</td>";
-      window.permissionResultContainer.appendChild(result);
+      document.getElementById(permissionName+"-p").innerHTML = permission.state;
     } catch (error) {
        console.log(error);
     }
 }
 
 function queryPermissions(){
-  window.permissionResultContainer = document.getElementById("permission-results");
   printPermission("geolocation");
   printPermission("notifications");
   printPermission("push", {userVisibleOnly: true});
