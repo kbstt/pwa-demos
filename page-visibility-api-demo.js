635d511a625bc993ee8efdb66ed61c7184eec9cf
kbstt <80958930+kbstt@users.noreply.github.com>
2025-11-17 09:12:38 +0800

    Create page-visibility-api-demo.js

---
+++ page-visibility-api-demo.js
@@ -0,0 +1,36 @@
+function initializePageVisibilityLog(){
+  var container = document.getElementById('page-visibility-log');
+  var hidden, visibilityChange;
+  if (typeof document.hidden !== "undefined") {
+    hidden = "hidden";
+    visibilityChange = "visibilitychange";
+  } 
+  else if (typeof document.mozHidden !== "undefined") {
+    hidden = "mozHidden";
+    visibilityChange = "mozvisibilitychange";
+  } 
+  else if (typeof document.msHidden !== "undefined") {
+    hidden = "msHidden";
+    visibilityChange = "msvisibilitychange";
+  } 
+  else if (typeof document.webkitHidden !== "undefined") {
+    hidden = "webkitHidden";
+    visibilityChange = "webkitvisibilitychange";
+  } 
+  else {
+    container.innerText = 'Page Visibility API not supported.';
+  }
+  
+  document.addEventListener(visibilityChange, function(){
+    var timeBadge = new Date().toTimeString().split(' ')[0];
+    var newState = document.createElement('p');
+    newState.innerHTML = '' + timeBadge + ' Page visibility changed to ' + (document[hidden] ? 'hidden' : 'visible') + '.';
+    container.appendChild(newState);
+  }, false);
+
+  if (hidden in document) {
+    document.getElementById('status').innerHTML = document[hidden] ? 'hidden' : 'visible';
+  }
+}
+
+window.addEventListener("load", initializePageVisibilityLog);
