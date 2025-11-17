cb7f60233d1d21cca8ba6c6dbcf0042bc4a423e7
kbstt <80958930+kbstt@users.noreply.github.com>
2025-11-17 14:17:34 +0800

    Create viewport-interactive-widget-resizing.js

---
+++ viewport-interactive-widget-resizing.js
@@ -0,0 +1,21 @@
+function updateViewportMetaTag(newResizing){
+  let meta = document.querySelector('meta[name="viewport"]);
+  let content = meta.getAttribute('content') || "";
+  let parts = content.split(',').map(p => p.trim()).filter(Boolean);
+  let filtered = parts.filter(p => !p.startsWith('interactive-widget'));
+  filtered.push("interactive-widget="+newResizing);
+  let newContent = filtered.join(', ');
+  meta.setAttribute('content', newContent);
+}
+
+function setResizesVisual(){
+   updateViewportMetaTag("resizes-visual");
+}
+
+function setResizeContent(){
+   updateViewportMetaTag("resizes-content");
+}
+
+function setOverlaysContent(){
+  updateViewportMetaTag("overlays-content");
+}
