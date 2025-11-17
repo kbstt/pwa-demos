7ff99eb81fcb1e0cccf4bd3c6af6f794924ebf1c
kbstt <80958930+kbstt@users.noreply.github.com>
2025-11-17 08:04:51 +0800

    Update permissions-api-demo.js

--- permissions-api-demo.js
+++ permissions-api-demo.js
@@ -1,22 +1,34 @@
+window.PermissionsList = ["geolocation", "notifications", "camera", "microphone", "background-sync", "ambient-light-sensor", "accelerometer", "gyroscope", "magnetometer"]
+    
 async function printPermission(permissionName, descriptor) {
     try {
-      let permission = await navigator.permissions.query(Object.assign({name: permissionName}, descriptor));
-      document.getElementById(permissionName+"-p").innerHTML = permission.state;
-    } catch (error) {
-       console.log(error);
+        let permission = await navigator.permissions.query(Object.assign({name: permissionName}, descriptor));
+        updatePermission(permissionName, permission.state);
+        
+        permission.addEventListener('change', function (e) {
+            updatePermission(permissionName, permission.state);
+        });
+    } 
+    catch (error) {
+        console.log(error);
     }
 }
 
 function queryPermissions(){
-  printPermission("geolocation");
-  printPermission("notifications");
-  printPermission("push", {userVisibleOnly: true});
-  printPermission("midi", {sysex: true});
-  printPermission("camera");
-  printPermission("microphone");
-  printPermission("background-sync");
-  printPermission("ambient-light-sensor");
-  printPermission("accelerometer");
-  printPermission("gyroscope");
-  printPermission("magnetometer");
+    window.PermissionsList.forEach(function(permission){
+        printPermission(permission);
+    });
 }
+
+function updatePermission(permissionName, permission.state){
+   document.getElementById(permissionName+"-p").innerHTML = permission.state;
+}
+
+function initializePermissionTable(){
+    let container = document.getElementById("permission-results");
+    window.PermissionsList.forEach(function(permission){
+        container.innerHTML += `<tr><td>`+permission+`</td><td id="`+permission+`-p"><pre>unknown</pre></td></tr>`
+    });
+}
+
+window.addEventListener("load", initializePermissionTable);
