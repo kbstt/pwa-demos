2971f6d5119fa058127c64aa25ed0fdec6229f5a
kbstt <80958930+kbstt@users.noreply.github.com>
2025-11-17 14:32:42 +0800

    Update viewport-interactive-widget-resizing.js

--- viewport-interactive-widget-resizing.js
+++ viewport-interactive-widget-resizing.js
@@ -8,14 +8,31 @@ function updateViewportMetaTag(newResizing){
   meta.setAttribute('content', newContent);
 }
 
-function setResizesVisual(){
+function setResizesVisual(e){
+   updateBtn(e);
    updateViewportMetaTag("resizes-visual");
 }
 
-function setResizeContent(){
+function setResizeContent(e){
+   updateBtn(e);
    updateViewportMetaTag("resizes-content");
 }
 
-function setOverlaysContent(){
+function setOverlaysContent(e){
+  updateBtn(e);
   updateViewportMetaTag("overlays-content");
+}
+
+function updateBtn(e){
+  let element = e.currentTarget;
+  document.querySelectorAll(".resizing-btn").forEach(function(btn){
+    if (btn === element){
+      btn.classList.remove("btn2");
+      btn.classList.add("btn1");
+    }
+    else {
+      btn.classList.remove("btn2");
+      btn.classList.add("btn2");
+    }
+  });
 }
