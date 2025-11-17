1c1eabf239d7fa17766a74808c310f00e04cf12a
kbstt <80958930+kbstt@users.noreply.github.com>
2025-11-17 14:26:38 +0800

    Update viewport-interactive-widget-resizing.js

--- viewport-interactive-widget-resizing.js
+++ viewport-interactive-widget-resizing.js
@@ -1,10 +1,10 @@
 function updateViewportMetaTag(newResizing){
-  let meta = document.querySelector('meta[name="viewport"]);
+  let meta = document.querySelector('meta[name="viewport"]');
   let content = meta.getAttribute('content') || "";
   let parts = content.split(',').map(p => p.trim()).filter(Boolean);
   let filtered = parts.filter(p => !p.startsWith('interactive-widget'));
   filtered.push("interactive-widget="+newResizing);
-  let newContent = filtered.join(', ');
+  let newContent = filtered.join(",");
   meta.setAttribute('content', newContent);
 }
 
