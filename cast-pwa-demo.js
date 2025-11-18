03c0496ce36a67009fda75f93d0fab612c3eed88
kbstt <80958930+kbstt@users.noreply.github.com>
2025-11-18 11:37:33 +0800

    Create cast-pwa-demo.js

---
+++ cast-pwa-demo.js
@@ -0,0 +1,33 @@
+async function setWebcam() {
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
+    window.videoSrcElement = document.getElementById("cast-demo-feed");
+    window.videoSrcElement.srcObject = stream;
+  } 
+  catch (err) {
+     alert("Couldn't access your camera");
+  }
+}
+
+function startCasting(){
+  if (!("presentation" in navigator)) {
+    alert ("Presentation API not supported");
+    return;
+  }
+
+  if (!window.videoSrcElement || !window.videoSrcElement.srcObject){
+    alert("You must start your camera first");
+  }
+
+  try {
+    const request = new PresentationRequest(['casting.html']);
+    presentationConnection = await request.start();
+    presentationConnection.send(JSON.stringify({ type: "camera" }));
+    const streamUrl = URL.createObjectURL(window.videoSrcElement.srcObject);
+    presentationConnection.send(JSON.stringify({ type: "stream", url: streamUrl }));
+  } 
+  catch (err) {
+      alert("Error starting presentation");
+      console.error();
+  }
+}
