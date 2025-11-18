992ee4cd016f3e90f8a18517bf2c97c9dc8f0cfa
kbstt <80958930+kbstt@users.noreply.github.com>
2025-11-18 15:10:21 +0800

    Update eye-dropper-api-demo.js

--- eye-dropper-api-demo.js
+++ eye-dropper-api-demo.js
@@ -3,4 +3,5 @@ async function openEyeDropper(){
   let color = await eyeDropper.open();
   let hex = color.sRGBHex;
   document.getElementById("color-input").value = hex;
+  document.getElementById("color-hex").innerHTML = hex;
 }
