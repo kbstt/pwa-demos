864902dfd4b7be4a5453d5c7464a60ca139b7001
kbstt <80958930+kbstt@users.noreply.github.com>
2025-11-18 14:44:20 +0800

    Create web-usb-demo.js

---
+++ web-usb-demo.js
@@ -0,0 +1,14 @@
+function connectToUSBDevice(){
+  try {
+    let log = document.getElementById('usb-log');
+    let device = await navigator.usb.requestDevice({ filters: [{}] });
+    await device.open();
+    log.textContent +=  "Device opened: \n";
+    log.textContent +=  "Manufacturer: "+device.manufacturerName + "\n";
+    log.textContent +=  "Product: "+device.productName + "\n";
+    log.textContent +=  "Serial Number: "+device.device.serialNumber + "\n";
+  } 
+  catch (err) {
+    console.error("Error:", err);
+  }
+}
