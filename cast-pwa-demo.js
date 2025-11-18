dbb3b35f33d3e9421da5d55778f95429df5f21de
kbstt <80958930+kbstt@users.noreply.github.com>
2025-11-18 12:03:22 +0800

    Update cast-pwa-demo.js

--- cast-pwa-demo.js
+++ cast-pwa-demo.js
@@ -1,34 +1,14 @@
-async function setWebcam() {
-  try {
-    const stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true});
-    window.videoSrcElement = document.getElementById("cast-demo-feed");
-    window.videoSrcElement.srcObject = stream;
-  } 
-  catch (err) {
-     alert("Couldn't access your camera");
-  }
-}
-
 async function startCasting(){
   if (!("presentation" in navigator)) {
     alert ("Presentation API not supported");
     return;
-  }
-
-  if (!window.videoSrcElement || !window.videoSrcElement.srcObject){
-    alert("You must start your camera first");
-  }
-
+  } 
   try {
-    const streamUrl = URL.createObjectURL(window.videoSrcElement.srcObject);
-    const request = new PresentationRequest([streamUrl]);
+    const request = new PresentationRequest(["http://127.0.0.1:5000/features/push-notifications"]);
     presentationConnection = await request.start();
-    presentationConnection.send(JSON.stringify({ type: "camera" }));
-    presentationConnection.send(JSON.stringify({ type: "stream", url: streamUrl }));
   } 
   catch (err) {
       alert("Error starting presentation");
-      alert(err);
-      console.error();
+      console.error(err);
   }
 }
