fb233967a23248870ff9e40a9f711565595a6028
kbstt <80958930+kbstt@users.noreply.github.com>
2025-11-20 06:59:09 +0800

    Create touch-gesture-pwa-demo.js

---
+++ touch-gesture-pwa-demo.js
@@ -0,0 +1,43 @@
+function startDrag(e) {
+  this.ontouchmove = this.onmspointermove = moveDrag;
+
+  this.ontouchend = this.onmspointerup = function () {
+    this.ontouchmove = this.onmspointermove = null;
+    this.ontouchend = this.onmspointerup = null;
+  }
+
+  var pos = [this.offsetLeft, this.offsetTop];
+  var that = this;
+  var origin = getCoors(e);
+
+  function moveDrag(e) {
+    var currentPos = getCoors(e);
+    var deltaX = currentPos[0] - origin[0];
+    var deltaY = currentPos[1] - origin[1];
+    this.style.left = (pos[0] + deltaX) + 'px';
+    this.style.top = (pos[1] + deltaY) + 'px';
+    return false; // cancels scrolling
+  }
+
+  function getCoors(e) {
+    var coors = [];
+    if (e.targetTouches && e.targetTouches.length) {
+      var thisTouch = e.targetTouches[0];
+      coors[0] = thisTouch.clientX;
+      coors[1] = thisTouch.clientY;
+    } else {
+      coors[0] = e.clientX;
+      coors[1] = e.clientY;
+    }
+    return coors;
+  }
+}
+
+var elements = document.querySelectorAll('.draggable-box');
+[].forEach.call(elements, function (element) {
+  element.ontouchstart = element.onmspointerdown = startDrag;
+});
+
+document.ongesturechange = function () {
+  return false;
+}
