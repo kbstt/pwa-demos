437ee974f8f9dd843a99716555145179bd735a50
kbstt <80958930+kbstt@users.noreply.github.com>
2025-11-20 08:57:11 +0800

    Update two-factor-sms-authentication-pwa.js

--- two-factor-sms-authentication-pwa.js
+++ two-factor-sms-authentication-pwa.js
@@ -1,4 +1,58 @@
-function sendCode(){ 
-  let otp = await navigator.credentials.get({otp: {transport: ["sms"]});
-  document.getElementById('#code-field').value = otp.code;
+async function startListeningToSMS() {
+  if (!window.OTPCredential){
+    alert("Your current device does not support window.OTPCredential");  
+    return;
+  }
+
+  try {
+    let ac = new AbortController();
+    let content = await navigator.credentials.get({
+      otp: { transport: ["sms"] },
+      signal: ac.signal
+    });
+
+    if (content && content.code) {
+      alert("Code detected from your SMS: "+content.code);
+    }
+    else {
+      alert("No code detected in your SMS");
+    }
+   } 
+  catch (err) {
+      console.error(err);
+      if (err.name === 'AbortError') {
+          alert("Listening stopped (Aborted).");
+      } 
+      else {
+          alert(`Error: ${err.message}`);
+      }
+  }
 }
+
+function generateRandomCode(){
+  window.randomCode = Math.floor(100000 + Math.random() * 900000);
+}
+
+function getSmsBody(){
+  //The browser's background process scans incoming SMS messages specifically looking for the last line to match this exact pattern:
+  //@ followed by your website's hostname (e.g., @my-app.com) # followed by the code (e.g., #123456)
+  let currentDomain = window.location.hostname;
+  let code = window.randomCode || generateRandomCode();
+  let smsBody = 'Your verification code is ' + randomCode + '.\n\n@' + currentDomain + ' #' + randomCode;
+  return smsBody;
+}
+
+function draftSms(){
+  window.open("sms:?body=" + encodeURIComponent(getSmsBody()), "_blank);
+}
+
+async function copySmsBodyToClipboard(){
+   await navigator.clipboard.writeText(getSmsBody());
+   alert("SMS text copied. Now send it yourself");
+}
+
+function generateSmsTxt(){
+  document.getElementById("plain-txt-sms").innerText = getSmsBody();
+}
+
+window.addEventListener("load", generateSmsTxt);
