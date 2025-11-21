4f559a8839a184134846128f46d669c4221344d0
kbstt <80958930+kbstt@users.noreply.github.com>
2025-11-21 15:37:26 +0800

    Update webrtc-pwa-demo.js

--- webrtc-pwa-demo.js
+++ webrtc-pwa-demo.js
@@ -53,8 +53,9 @@ async function finalizeConnection() {
 
 function copyFromTextarea(event){
   let element = event.currentTarget;
-  element.select();
   let content = element.value;
+  if (!content)(return;}
+  element.select();
   navigator.clipboard.writeText(content);
   alert("Copied! Now paste it in your other tab.");
 }
