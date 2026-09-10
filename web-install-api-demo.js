14c527a766f59f13120bbbf55f0a6c3e905b2ea6
kbstt <80958930+kbstt@users.noreply.github.com>
2026-09-10 10:39:15 +0800

    Update web-install-api-demo.js

--- web-install-api-demo.js
+++ web-install-api-demo.js
@@ -11,26 +11,26 @@ async function installApp(){
   } 
   catch (err) {
     switch (err.name) {
-      case "AbortError":
-        el.innerHTML = "AbortError: install was cancelled or could not be completed.";
-        break;
-      case "DataError":
-        el.innerHTML = "DataError: manifest or manifest ID is invalid.";
-        break;
-      case "InvalidStateError":
-        el.innerHTML = "InvalidStateError: install attempt made from sandboxed frame or cross-origin subframe";
-        break;
-      case "NotAllowedError":
-        el.innerHTML = "NotAllowedError: install attempt lacking transient user activation or disallowed by browser policy.";
-        break;
-      case "NotFoundError":
-        el.innerHTML = "NotFoundError: navigator no longer attached to a document";
-        break;
-      case "TypeError":
-        el.innerHTML = "TypeError: argument with invalid type or URL.";
-        break;
-		  default:
-        el.innerHTML = "UnknownError: The install could not be completed for an unknown reason";
+		case "AbortError":
+        	el.innerHTML = "AbortError: install was cancelled or could not be completed.";
+        	break;
+		case "DataError":
+        	el.innerHTML = "DataError: manifest or manifest ID is invalid.";
+        	break;
+		case "InvalidStateError":
+        	el.innerHTML = "InvalidStateError: install attempt made from sandboxed frame or cross-origin subframe";
+        	break;
+		case "NotAllowedError":
+        	el.innerHTML = "NotAllowedError: install attempt lacking transient user activation or disallowed by browser policy.";
+        	break;
+		case "NotFoundError":
+        	el.innerHTML = "NotFoundError: navigator no longer attached to a document";
+        	break;
+		case "TypeError":
+        	el.innerHTML = "TypeError: argument with invalid type or URL.";
+        	break;
+		default:
+        	el.innerHTML = "UnknownError: The install could not be completed for an unknown reason";
       }
   }
 }
