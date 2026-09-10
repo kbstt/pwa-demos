e82052a13e41486e0cf6c3f505527ee8889d788e
kbstt <80958930+kbstt@users.noreply.github.com>
2026-09-10 10:33:55 +0800

    Update web-install-api-demo.js

--- web-install-api-demo.js
+++ web-install-api-demo.js
@@ -1,19 +1,36 @@
-function installApp(){
-  if (!navigator.install) {return; } // API not supported
+async function installApp(){
+  let el = document.getElementById("installResult");
   try {
-    //You can install the current domain or any other PWA with a manifest
-    const installUrl = "https://progressier.com/login";
-    await navigator.install(installUrl);
+    if ("install" in navigator){
+      await navigator.install();
+		  el.innerHTML = "Success: Install completed";
+    }
+    else {
+      el.innerHTML = "Incompatible: navigator.install() is not supported in your current browser";
+    }
   } 
   catch (err) {
-    if (err.name === "AbortError"){
-      // The user dismissed the prompt
-    }
-    else if (err.name === "DataError"){
-      // There was an issue with the manifest or the installation domain
-    }
-    else{
-      //Other type of error
-    }
+    switch (err.name) {
+      case "AbortError":
+        el.innerHTML = "AbortError: install was cancelled or could not be completed.";
+        break;
+      case "DataError":
+        el.innerHTML = "DataError: manifest or manifest ID is invalid.";
+        break;
+      case "InvalidStateError":
+        el.innerHTML = "InvalidStateError: install attempt made from sandboxed frame or cross-origin subframe";
+        break;
+      case "NotAllowedError":
+        el.innerHTML = "NotAllowedError: install attempt lacking transient user activation or disallowed by browser policy.";
+        break;
+      case "NotFoundError":
+        el.innerHTML = "NotFoundError: navigator no longer attached to a document";
+        break;
+      case "TypeError":
+        el.innerHTML = "TypeError: argument with invalid type or URL.";
+        break;
+		  default:
+        el.innerHTML = "UnknownError: The install could not be completed for an unknown reason";
+      }
   }
-};
+}
